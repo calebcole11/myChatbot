6c3b6b89987b091a0534442bffa4d3f62e90a025
Caleb Cole <calebcole121@gmail.com>
2021-09-29 09:34:05 -0600

    added stubs for political and polite, integrated methods from when I was gone.

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -1,32 +1,114 @@
 package chat.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+
 public class Chatbot
 {
 	private String name;
+	private int greetingCount;
+	private int farewellCount;
 	
 	
 	public Chatbot(String name)
 	{
 		this.name = name;
+		this.greetingCount = 0;
+		this.farewellCount=4;
 	}
 	
 	public String processText(String text)
 	{
-		String response = "You said: ";
+		String response = sayGreeting() + "\nYou said: ";
 		
 		response += text + "\n";
 		
+		if (text.toLowerCase().indexOf("date") >= 0 )
+		{
+			response += getDate();
+		}
+		
+		if (text.toUpperCase().indexOf("TIME") >= 0)
+		{
+			response += getTime();
+		}
+	
+		
+		response += sayFarewell();
 	
 		return response;
 	}
+	public String sayGreeting()
+	{
+		String greeting = "";
+		
+		ArrayList<String> greetings = new ArrayList<String>();
+		
+		greetings.add("Hi there!");
+		greetings.add("Aloha");
+		greetings.add("Greetings");
+		greetings.add("");
+		greetings.add("");
+		
+		greeting = greetings.get(greetingCount);
+		greetingCount++;
+		if (greetingCount == greetings.size())
+		{
+			greetingCount = 0;
+		}
+		return greeting;
+	}	
 	
-	@Override
+	public String sayFarewell()
+	{
+		String farewell = "";
+		
+		ArrayList<String> farewells = new ArrayList<String>();
+		
+		farewells.add(farewells.size(), "Goodbye");
+		farewells.add("sayonara");
+		farewells.add("Catch you later");
+		farewells.add("Bye!");
+		farewells.add("See you later!");
+		
+		farewell = farewells.get(farewellCount);
+		farewellCount--;
+		if (farewellCount < 0)
+		{
+			farewellCount = farewells.size() -1;
+		}
+		
+		return farewell;
+	}
+	
+	public boolean isPolite(String content)
+	{
+		boolean isValid = false;
+		
+		return isValid;
+	}
+	
+	public boolean isPolitical(String words)
+	{
+		boolean isValid = false;
+		
+		return isValid;
+	}
+	
 	public String toString()
 	{
-		String description = "I am a Chatbot, my name is: " + name + "\n";
-		description += "My favorite topic is: coming soon";
 		
-		return description;
+	}
+	
+	public String getDate()
+	{
+		
+	}
+	
+	public String getTime()
+	{
+		
 	}
 
 }
